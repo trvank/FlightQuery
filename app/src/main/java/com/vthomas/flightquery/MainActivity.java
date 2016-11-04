bcf6dea7eef0ba9d4a7f69ede99b2ac66494b060
trvanklav <vthomas@pdx.edu>
2016-11-03 18:56:54 -0700

    added ai.api integration.  Basic natural language capabilities on main activity

--- app/src/main/java/com/vthomas/flightquery/MainActivity.java
+++ app/src/main/java/com/vthomas/flightquery/MainActivity.java
@@ -1,52 +1,111 @@
 package com.vthomas.flightquery;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import ai.api.AIConfiguration;
+import ai.api.AIListener;
+import ai.api.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import com.google.gson.JsonElement;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements AIListener{
+    String[] perms = {"android.permission.RECORD_AUDIO"};
+    int permsRequestCode = 200;
+
+    private AIService aiService;
+    private Button listenButton;
+    private TextView resultTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        final AIConfiguration config = new AIConfiguration("bba91e5348ff4dc7a964696d4daae63a",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        listenButton = (Button) findViewById(R.id.listenButton);
+        resultTextView = (TextView) findViewById(R.id.resultText);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //request permission for recording audio if not yet granted
+        if(!hasPermission(perms[0])){
+            requestPermissions(perms, permsRequestCode);
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResult(AIResponse response) {
+        Result result = response.getResult();
+
+        //Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()){
+            for(final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()){
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ")";
+            }
+        }
+
+        //Show results in TextView
+        resultTextView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString);
+    }
+
+    @Override
+    public void onError(AIError error) {
+        resultTextView.setText(error.toString());
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+
+    //action for the listen button
+    public void listen(View view) {
+        aiService.startListening();
+    }
+
+    //does the device need record permission???
+    private boolean needsPermission(){
+        return(Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    private boolean hasPermission(String permission){
+        if(needsPermission()){
+            return(checkSelfPermission(permission)== PackageManager.PERMISSION_GRANTED);
+        }
+        return true;
     }
 }
